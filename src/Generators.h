3ad0318671cd4a938659ce9f0de8c7339b5c712b
sebasmfonseca <127047660+sebasmfonseca@users.noreply.github.com>
2023-03-22 18:01:25 +0000

    Update Generators.h

--- src/Generators.h
+++ src/Generators.h
@@ -11,8 +11,8 @@ public:
   double Random_Distribution(double ,double ,TF1* ); //generates a random number according to a distribution
   vector<double> Generate_Position(double,double,double); //Genertes a muon's starting position
   vector<double> Random_Distribution_2D(TF1*,double,double,double,double,double); //Generates 2 random numbers according to a pdf
-  double Generate_Photon_Energy(TSpline3*); //Generates poton energy according to an interpolation, use tools::Interpolate_Photon_Spectrum to get the distribution
-  int Generate_Photon_Number(double); //Generates and integer according to a Poisson distribution arround the input
+  double Generate_Photon_Energy(TSpline3*); //Generates photon energy according to an interpolation of a pdf, use tools::Interpolate_Photon_Spectrum to get the distribution
+  int Generate_Photon_Number(double); //Generates an integer according to a Poisson distribution arround the input
 
   ~Generator();
 
