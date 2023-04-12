0114e9f0e7fca1a6696073c1f7717487dd9f6f48
Guilherme Gaspar <guilherme.gaspar@tecnico.ulisboa.pt>
2023-04-12 04:19:02 +0100

    Tafc_Project updated

---
+++ Tafc_Project/Tafc_Project/main/main.C
@@ -0,0 +1,199 @@
+#include "Generators.h"
+#include "Geometry.h"
+#include "tools.h"
+#include "Particle.h"
+#include "Tracker.h"
+#include "DataManager.h"
+#include "TApplication.h"
+#include <string>
+#include <chrono>
+//#include <thread>
+using namespace std;
+
+/*
+//function for multithreading
+void multiple_muons(Tracker *T)
+{
+  T->Propagate_Muon();
+  if(T->GetDoubleCross())
+  {
+    T->Propagate_Photons(T->GetN_photons());
+    //Data->Extract_Data(T,i);
+  }
+  delete T;
+}*/
+
+int main(int argc, char* argv[])
+{
+  //int t0 = time(0),t1=0;
+  auto start = std::chrono::high_resolution_clock::now();
+
+  double radius = 5.0;
+  double height = 1.0;
+  double distance = 25.;
+  double airgap = .1;
+  double althickness = 0.0016;
+  double step = 0.001;
+  int n_SIPMS = 4;
+  double SIPM_size = .6;
+
+  //int nThreads, tid;
+
+  //vector <thread> threads;
+
+  if(argc == 1) //Simulates a single muon
+  {
+    Generator* gen = new Generator();
+    for(int i=0;i<10;i++)
+    {
+    Tracker* T = new Tracker(radius,height,distance,airgap,althickness,step,gen,n_SIPMS,SIPM_size);
+    T->Propagate_Muon();
+    T->Propagate_Photons(T->GetN_photons());
+
+    cout<<endl<<"Total Photons Generated: "<<T->GetN_photons()<<endl;
+    cout<<"Photons Absorbed: "<<T->GetN_absorbed()<<endl;
+    cout<<"Photons Detected: "<<T->GetN_detected()<<endl;
+    cout<<"Photons Lost: "<<T->GetN_lost()<<endl;
+    delete T;
+    }
+    //t1 = time(0);
+    //cout<<"Time elapsed: "<<t1-t0<<" seconds"<<endl;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> duration = end - start;
+    std::cout << duration.count() << "s " << std::endl;
+
+    cout<<"Muons Simulated: "<<1<<endl;
+    return 0;
+  }
+
+  if(argc == 2) //Simulates n muons
+  {
+    int n_muons=0;
+    if(sscanf(argv[1],"%d",&n_muons))
+    {
+      DataManager *Data = new DataManager(n_muons);
+      int N_photons=0,N_absorbed=0,N_detected=0,N_lost=0;
+      Generator* gen = new Generator();
+      /*
+      //create multiple threads with the muons
+      for(int i=0;i<n_muons;i++)
+      {
+        cout<<endl<<"Muon: "<<i<<endl<<endl;
+        Tracker* T = new Tracker(radius,height,distance,airgap,althickness,step,gen,n_SIPMS,SIPM_size);
+        thread t(multiple_muons, T);
+        threads.push_back(move(t));
+      }*/
+      for(int i=0;i<n_muons;i++)
+      {
+        cout<<endl<<"Muon: "<<i<<endl<<endl;
+
+        Tracker* T = new Tracker(radius,height,distance,airgap,althickness,step,gen,n_SIPMS,SIPM_size);
+        T->Propagate_Muon();
+        if(T->GetDoubleCross())
+        {
+          T->Propagate_Photons(T->GetN_photons());
+          Data->Extract_Data(T,i);
+        }
+        delete T;
+      }
+      /*
+      //wait for threads to finish before carrying on
+      for(auto &t: threads){
+          t.join();
+        }*/
+      for(int i=0;i<n_muons;i++)
+      {
+        Data->Print_Data(i);
+      }
+      Data->Draw_Efficiency_Graph();
+      //t1 = time(0);
+      //cout<<"Time elapsed: "<<t1-t0<<" seconds"<<endl;
+
+      auto end = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<float> duration = end - start;
+      std::cout << duration.count() << "s " << std::endl;
+
+      cout<<"Muons Simulated: "<<n_muons<<endl;
+      return 0;
+    }
+  }
+
+  if(argc == 2 && !strcmp(argv[1],"-heatmap")) //Debug mode - Prints information about every photon
+  {
+
+    Generator* gen = new Generator();
+
+    Tracker* T = new Tracker(radius,height,distance,airgap,althickness,step,gen,n_SIPMS,SIPM_size);
+
+    T->Propagate_Muon();
+    T->Heatmap();
+
+    return 0;
+
+  }
+
+  if(argc == 3 && !strcmp(argv[1],"-draw")) //Draw mode - Single Muon and draw n photons
+  {
+    int n=0;
+    if(sscanf(argv[2],"%d",&n))
+    {
+      Generator* gen = new Generator();
+
+      Tracker* T = new Tracker(radius,height,distance,airgap,althickness,step,gen,n_SIPMS,SIPM_size);
+
+      T->Propagate_Muon();
+      T->Propagate_Photons(n);
+
+      cout<<endl<<"Total Photons Generated: "<<T->GetN_photons()<<endl;
+      cout<<"Photons Absorbed: "<<T->GetN_absorbed()<<endl;
+      cout<<"Photons Detected: "<<T->GetN_detected()<<endl;
+      cout<<"Photons Lost: "<<T->GetN_lost()<<endl;
+
+      auto end = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<float> duration = end - start;
+      std::cout << duration.count() << "s " << std::endl;
+
+
+      TApplication app("app", nullptr, nullptr);
+      T->Draw();
+      app.Run();
+
+
+      return 0;
+    }
+
+  }
+
+  if(argc == 3 && !strcmp(argv[1],"-seed")) //Seed mode - Single Muon with a given seed
+  {
+    int seed=0;
+    if(sscanf(argv[2],"%d",&seed))
+    {
+      Generator* gen = new Generator(seed);
+
+      Tracker* T = new Tracker(radius,height,distance,airgap,althickness,step,gen,n_SIPMS,SIPM_size);
+
+      T->Propagate_Muon();
+      T->Propagate_Photons(T->GetN_photons());
+
+      cout<<endl<<"Total Photons Generated: "<<T->GetN_photons()<<endl;
+      cout<<"Photons Absorbed: "<<T->GetN_absorbed()<<endl;
+      cout<<"Photons Detected: "<<T->GetN_detected()<<endl;
+      cout<<"Photons Lost: "<<T->GetN_lost()<<endl;
+
+      //t1 = time(0);
+      //cout<<"Time elapsed: "<<t1-t0<<" seconds"<<endl;
+      auto end = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<float> duration = end - start;
+      std::cout << duration.count() << "s " << std::endl;
+
+      cout<<"Muons Simulated: "<<1<<endl;
+      return 0;
+    }
+
+  }
+
+  cout<<"Invalid Input"<<endl;
+  return 1;
+}
